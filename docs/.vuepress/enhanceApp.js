6c51ebe3bcf43f27fe947d8b415545477ff34897
Paul Ang <punggolzenith@gmail.com>
2021-06-02 16:16:01 +0800

    add bootstrap and git ignore

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default ({
+  Vue,
+}) => {
+    Vue.use(BootstrapVue)
+}
