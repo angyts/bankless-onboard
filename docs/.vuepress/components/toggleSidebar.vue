1c0bbb68d7e3307193880a7db14e3058263cb8e9
Paul Ang <punggolzenith@gmail.com>
2021-06-03 00:01:38 +0800

    added components

---
+++ docs/.vuepress/components/toggleSidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container class="theme-container p-2">
+      <div>
+        <b-button v-b-toggle.sidebar-right>Show Current Lesson Plan</b-button>
+        <b-sidebar id="sidebar-right" title="Sidebar" right shadow>
+          <div class="px-3 py-2">
+            <p>
+              Lesson 1
+            </p>
+            <p>
+              Lesson 2
+            </p>
+            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+          </div>
+        </b-sidebar>
+      </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "toggleSidebar"
+}
+</script>
+
+<style scoped>
+
+</style>
--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,69 +1,18 @@
 <template>
-  <b-container class="theme-container p-2">
-
-      <div>
-        <b-navbar toggleable="lg" type="dark" variant="dark">
-          <b-navbar-brand href="#">Bankless: Onboard</b-navbar-brand>
-
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-item href="#">Lesson 1</b-nav-item>
-              <b-nav-item href="#">Lesson 2</b-nav-item>
-            </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-              <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-              </b-nav-form>
-
-              <b-nav-item-dropdown text="Lang" right>
-                <b-dropdown-item href="#">EN</b-dropdown-item>
-                <b-dropdown-item href="#">ES</b-dropdown-item>
-                <b-dropdown-item href="#">RU</b-dropdown-item>
-                <b-dropdown-item href="#">FA</b-dropdown-item>
-              </b-nav-item-dropdown>
-
-              <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template #button-content>
-                  <em>Connect Wallet</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
-      </div>
-
-      <div>
-        <b-button v-b-toggle.sidebar-right>Show Current Lesson Plan</b-button>
-        <b-sidebar id="sidebar-right" title="Sidebar" right shadow>
-          <div class="px-3 py-2">
-            <p>
-              Lesson 1
-            </p>
-            <p>
-              Lesson 2
-            </p>
-            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
-          </div>
-        </b-sidebar>
-      </div>
-
-    <b-container>
+  <b-container class="p-2">
+      <navigation-bar></navigation-bar>
       <Content/>
-    </b-container>
-
+      <toggle-sidebar></toggle-sidebar>
   </b-container>
 </template>
 
 <script>
+import navigationBar from "../components/navigationBar";
+import toggleSidebar from "../components/toggleSidebar";
 
+export default{
+  components:{navigationBar, toggleSidebar},
+}
 </script>
 
 <style>
