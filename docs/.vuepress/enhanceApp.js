8b60a327b022581a66168f380bb2fd1d4101f20b
Paul Ang <punggolzenith@gmail.com>
2021-06-03 02:30:59 +0800

    tried to use idx

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,11 +1,21 @@
-import Vue from 'vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import Ceramic from '@ceramicnetwork/http-client'
+import { IDX } from '@ceramicstudio/idx'
+
+const aliases = {
+  alias1: 'definitionID 1',
+  alias2: 'definitionID 2',
+}
+
+const ceramic = new Ceramic('https://ceramic-clay.3boxlabs.com')
+const idx = new IDX({ ceramic, aliases })
+
 export default ({
-  Vue,
+  Vue, idx
 }) => {
     Vue.use(BootstrapVue)
 }
